ca177fc0b797ee0e053814285ea6a873f3a53943
Min-ho Kim <xho1995@gmail.com>
2021-01-11 19:51:58 +0900

    initializes the package

--- Sources/PackageHigher/PackageHigher.swift
+++ Sources/PackageHigher/PackageHigher.swift
@@ -1,3 +1,16 @@
-struct PackageHigher {
-    var text = "Hello, World!"
+import PackageLower
+
+public struct PackageHigher {
+    var lower = PackageLower()
+    var text: String
+    
+    public init(lower: PackageLower = PackageLower(),
+                text: String = "Hello, World!") {
+        self.lower = lower
+        self.text = text
+    }
+    
+    public func greet() {
+        print("\(lower.getGreeting())")
+    }
 }
